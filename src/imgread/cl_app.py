632ab56b494c8cb2a996a8d707fcdd4aef16afba
ayasyrev <a.yasyrev@gmail.com>
2022-09-13 15:11:05 +0300

    cl

--- src/imgread/cl_app.py
+++ src/imgread/cl_app.py
@@ -1,9 +1,26 @@
-from .img_libs import img_lib_available
+from pathlib import Path
+import typer
+
 from .version import __version__
+from .read_image import img_libs
 
 
-def cl_app() -> None:
-    print(f"imgread version: {__version__}")
-    print("Availible image libs:")
-    for img_lib in img_lib_available:
-        print(" " * 4, img_lib)
+app = typer.Typer()
+
+
+@app.command()
+def imgread(
+    version: bool = typer.Option(
+        False, "-v", "--version", help="App version and img libs available."),
+) -> None:
+    print("imgread.")
+    if version:
+        print(f"version: {__version__}")
+        print("Availible image libs:")
+        max_len = max(len(lib_name) for lib_name in img_libs)
+        for img_lib in img_libs:
+            print(f"    {img_lib:{max_len}} {img_libs[img_lib].__version__}")
+
+
+if __name__ == "__main__":  # pragma: no cover
+    app()
