f0088b70e0de3f65cd320d39177f03f28535e3e8
ayasyrev <a.yasyrev@gmail.com>
2022-09-15 12:00:15 +0300

    pyvips

--- src/imgread/img_libs/pyvips.py
+++ src/imgread/img_libs/pyvips.py
@@ -7,17 +7,13 @@ __version__ = pyvips.__version__
 
 
 def read_img(img_path: str) -> np.ndarray:
-    image = pyvips.Image.new_from_file(img_path, access="sequential", memory=True)
+    # image = pyvips.Image.new_from_file(img_path, access="sequential", memory=True)
+    return pyvips.Image.new_from_file(img_path, access="sequential", memory=True)
 
-    numpy_image = np.ndarray(
-        dtype=np.uint8, shape=[image.height, image.width, image.bands]  # type: ignore
-    )
 
-    return numpy_image
+def read_img_nparray(img_path: str) -> np.ndarray:
+    return np.asarray(read_img(img_path))
 
 
 def read_img_pil(img_path: str) -> Image.Image:
-    return Image.fromarray(read_img(img_path))
-
-
-read_img_nparray = read_img
+    return Image.fromarray(read_img_nparray(img_path))
