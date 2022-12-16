4a62e0d5c183354bc79b8462c3ceea24480d1db5
ayasyrev <a.yasyrev@gmail.com>
2022-12-16 13:40:50 +0000

    bump 0.1.2

--- src/imgread/read_img.py
+++ src/imgread/read_img.py
@@ -6,7 +6,7 @@ from typing import Any, Callable, Dict
 from numpy import ndarray
 from PIL import Image
 
-from .img_libs import img_lib_available
+from .img_libs.img_libs_pkgs import img_lib_available
 
 
 def load_lib(lib_name: str = "pil", module_name: str = "imgread.img_libs") -> Any:
@@ -21,17 +21,16 @@ for img_lib in img_lib_available:
     img_libs[img_lib] = load_lib(img_lib)
 
 
-def get_func_dict(func_name: str, img_libs=img_libs):
+def get_func_dict(func_name: str, func_dict: dict) -> dict:
+    """Return dict lib_name: func for given func_name"""
     return {
-        # lib_name: func
-        lib_name: getattr(img_libs[lib_name], func_name)  # for colab: python 3.7
+        lib_name: func
         for lib_name in img_lib_available
-        # if (func := getattr(img_libs[lib_name], func_name, None)) is not None
-        if getattr(img_libs[lib_name], func_name, None) is not None
+        if (func := getattr(func_dict[lib_name], func_name, None)) is not None
     }
 
 
-read_img: Dict[str, Callable[[str], Any]] = get_func_dict("read_img")
-read_img_pil: Dict[str, Callable[[str], Image.Image]] = get_func_dict("read_img_pil")
-read_img_nparray: Dict[str, Callable[[str], ndarray]] = get_func_dict("read_img_nparray")
-read_img_version: Dict[str, str] = get_func_dict("__version__")
+read_img: Dict[str, Callable[[str], Any]] = get_func_dict("read_img", img_libs)
+read_img_pil: Dict[str, Callable[[str], Image.Image]] = get_func_dict("read_img_pil", img_libs)
+read_img_nparray: Dict[str, Callable[[str], ndarray]] = get_func_dict("read_img_nparray", img_libs)
+read_img_version: Dict[str, str] = get_func_dict("__version__", img_libs)
