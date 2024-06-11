ac6b4c5e787f8e304a4433f9a1790c94037b67b3
ayasyrev <a.yasyrev@gmail.com>
2024-06-11 09:31:53 +0000

    rename to_format to target_format, as property

--- src/imgread/benchmark.py
+++ src/imgread/benchmark.py
@@ -1,11 +1,11 @@
-from typing import Callable, Dict, List, Optional
+from typing import Callable, Dict, List, Literal, Optional
 
 from benchmark_utils import BenchmarkIter
 
 from .get_img_filenames import get_img_filenames
 from .read_img import read_img, read_img_ndarray, read_img_pil
 
-read_to_format = {
+READ_TO_FORMAT = {
     "def": read_img,
     "pil": read_img_pil,
     "np": read_img_ndarray,
@@ -21,14 +21,14 @@ class BenchmarkImgRead(BenchmarkIter):
         self,
         img_path: Optional[str] = None,
         num_samples: int = 0,
-        to_format: str = "def",
+        target_format: Literal["def", "pil", "np"] = "def",
         func_dict: Optional[Dict[str, Callable]] = None,
         filenames: Optional[List[str]] = None,
         num_repeats: int = 5,
         clear_progress: bool = False,
     ):
-        self.to_format = to_format
-        func_to_test = func_dict or read_to_format[to_format]
+        self._target_format = target_format
+        func_to_test = func_dict or READ_TO_FORMAT[target_format]
         img_path = img_path or "."
         if filenames is None:
             filenames = get_img_filenames(img_path, num_samples=num_samples)
@@ -39,14 +39,19 @@ class BenchmarkImgRead(BenchmarkIter):
             clear_progress=clear_progress,
         )
 
-    def set_func_dict(self, to_format: str) -> None:
-        if to_format not in read_to_format:
-            print(f"{to_format} not in available format.")
-            print("available: ", ", ".join(read_to_format.keys()))
+    @property
+    def target_format(self) -> Literal["def", "pil", "np"]:
+        return self._target_format
+
+    @target_format.setter
+    def target_format(self, target_format: Literal["def", "pil", "np"]) -> None:
+        if target_format not in READ_TO_FORMAT:
+            print(f"{target_format} not in available format.")
+            print("available: ", ", ".join(READ_TO_FORMAT.keys()))
         else:
-            self.to_format = to_format
-            self.func_dict = read_to_format[to_format]
+            self._target_format = target_format
+            self.func_dict = READ_TO_FORMAT[target_format]
 
     @property
     def func_names(self) -> str:
-        return f"to {self.to_format}: {', '.join(self.func_dict.keys())}"
+        return f"target_format: {self.target_format}, image libs: {', '.join(self.func_dict.keys())}"
