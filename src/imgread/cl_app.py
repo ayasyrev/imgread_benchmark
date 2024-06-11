c532f7fafbee7c8db3ef2330263134857e423cfb
ayasyrev <a.yasyrev@gmail.com>
2024-06-11 11:58:54 +0000

    refactor apps - argparsecfg instead of typer

--- src/imgread/cl_app.py
+++ src/imgread/cl_app.py
@@ -1,24 +1,35 @@
-import typer
+from argparsecfg import field_argument
+from argparsecfg.app import app
+from dataclasses import dataclass
 
 from .read_img import img_libs
 from .version import __version__
 
-app = typer.Typer()
+
+@dataclass
+class AppConfig:
+    version: bool = field_argument(
+        "-v", "--version", help="App version.", default=False
+    )
 
 
-@app.command()
+@app(
+    description="Imgread. Helpers utils for check and benchmark libs for read image files.",
+)
 def imgread(
-    version: bool = typer.Option(False, "-v", "--version", help="App version."),
+    cfg: AppConfig,
 ) -> None:
     """Imgread. Helpers utils for check and benchmark libs for read image files."""
     print("Imgread. Helpers utils for check and benchmark libs for read image files.")
-    print(f"Available {len(img_libs)} image libs:")
-    max_len = max(len(lib_name) for lib_name in img_libs)
-    for img_lib in img_libs:
-        print(f"    {img_lib:{max_len}} {img_libs[img_lib].__version__}")
-    if version:
+    print(f"Available {len(img_libs)}s image libs:")
+
+    if cfg.version:
         print(f"version: {__version__}")
+    else:
+        max_len = max(len(lib_name) for lib_name in img_libs)
+        for img_lib in img_libs:
+            print(f"    {img_lib:{max_len}} {img_libs[img_lib].__version__}")
 
 
 if __name__ == "__main__":  # pragma: no cover
-    app()
+    imgread()
--- src/imgread/cl_bench.py
+++ src/imgread/cl_bench.py
@@ -1,70 +1,83 @@
 from pathlib import Path
-from typing import Literal
-
-import typer
-
+import sys
+from argparsecfg import field_argument
+from argparsecfg.app import app
+from dataclasses import dataclass
 from .benchmark import BenchmarkImgRead
 from .get_img_filenames import get_img_filenames
 
-app = typer.Typer()
 
-
-@app.command()
-def benchmark(
-    img_path: Path = typer.Argument(..., help="Directory with images for benchmark"),
-    num_samples: int = typer.Option(
-        200, "-n", "--num_samples", help="Number of samples for test, default 200."
-    ),
-    target_format: Literal["def", "pil", "np"] = typer.Option(
-        "def",
+@dataclass
+class AppConfig:
+    img_path: str = field_argument(
+        "img_path",  # positional argument
+        help="Directory with images for benchmark",
+    )
+    num_samples: int = field_argument(
+        "-n",
+        default=200,
+        help="Number of samples for test, default 200.",
+    )
+    to: str = field_argument(
         "-t",
-        "--to",
+        default="def",
         help="Format for read image to: default: 'def', Pil: 'pil', or Numpy: 'np'.",
-    ),
-    all: bool = typer.Option(False, "-A", "--all", help="Use all images from folder"),
-    img_lib: str = typer.Option(None, "-l", "--img_lib", help="Image lib to test"),
-    exclude_lib: str = typer.Option(
-        None,
+    )
+    all: bool = field_argument(
+        "-A", default=False, action="store_true", help="Use all images from folder"
+    )
+    img_lib: str = field_argument(
+        "-l", "--img_lib", default=None, help="Image lib to test"
+    )
+    exclude: str = field_argument(
         "-x",
-        "--exclude",
+        default=None,
         help="Image lib exclude from test",
-    ),
-    multiprocessing: bool = typer.Option(
-        False, "-m", help="use multiprocessing, default=False"
-    ),
-    num_workers: int = typer.Option(
-        None, "--nw", help="num workers, if 0 -> use all cpus"
-    ),
+    )
+    multiprocessing: bool = field_argument(
+        "-m",
+        default=False,
+        action="store_true",
+        help="use multiprocessing, default=False",
+    )
+    nw: int = field_argument(default=None, help="num workers, if 0 -> use all cpus")
+
+
+@app(
+    description="Benchmark read image functions.",
+)
+def benchmark(
+    cfg: AppConfig,
 ) -> None:
     """Benchmark read image functions."""
-    if not img_path.exists():
-        typer.echo(f"Img dir {img_path} dos not exist!")
-        raise typer.Exit()
-    if all:
-        num_samples = 0
-    filenames = get_img_filenames(img_path, num_samples=num_samples)
-    if len(filenames) < num_samples:
-        typer.echo(
-            f"! Number of files in {img_path}: {len(filenames)} less than num_samples: {num_samples}"
+    if not Path(cfg.img_path).exists():
+        print(f"Img dir {cfg.img_path} dos not exist!")
+        raise sys.exit()
+    if cfg.all:
+        cfg.num_samples = 0
+    filenames = get_img_filenames(cfg.img_path, num_samples=cfg.num_samples)
+    if len(filenames) < cfg.num_samples:
+        print(
+            f"! Number of files in {cfg.img_path}: {len(filenames)} less than num_samples: {cfg.num_samples}"
         )
 
-    print(f"Benchmarking with images from {img_path}, convert to: {target_format}")
-    if num_samples:
-        print(f"number of samples: {num_samples}")
+    print(f"Benchmarking with images from {cfg.img_path}, target format: {cfg.to}")
+    if cfg.num_samples:
+        print(f"number of samples: {cfg.num_samples}")
     else:
         print(f"{len(filenames)} images.")
 
     bench = BenchmarkImgRead(
         filenames=filenames,
-        target_format=target_format,
+        target_format=cfg.to,
     )
     bench.run(
-        func_name=img_lib,
-        exclude=exclude_lib,
-        multiprocessing=multiprocessing,
-        num_workers=num_workers,
+        func_name=cfg.img_lib,
+        exclude=cfg.exclude,
+        multiprocessing=cfg.multiprocessing,
+        num_workers=cfg.nw,
     )
 
 
 if __name__ == "__main__":  # pragma: no cover
-    app()
+    benchmark()
