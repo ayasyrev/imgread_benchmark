c9168bac4b4bae3e9f79ed19e27d6cc61ac8b9d8
ayasyrev <a.yasyrev@gmail.com>
2024-07-08 10:29:31 +0000

    uv, ruff to nox

--- noxfile.py
+++ noxfile.py
@@ -4,9 +4,8 @@ import nox
 @nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
 def tests(session):
     args = session.posargs or ["--cov"]
-    session.install(
-        ".",
-        "pytest",
-        "pytest-cov",
-    )
+    session.install("uv")
+    session.run("uv", "pip", "install", ".[test]")
+    session.run("uv", "pip", "install", "torchvision")
+
     session.run("pytest", *args)
