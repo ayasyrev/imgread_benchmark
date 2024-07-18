a8bd7c85358341dbd68c6e31a5c725c04f92050b
ayasyrev <a.yasyrev@gmail.com>
2024-07-18 17:23:21 +0300

    nox_file fixed

--- noxfile.py
+++ noxfile.py
@@ -1,11 +1,8 @@
 import nox
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
+@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"], venv_backend="uv")
 def tests(session):
     args = session.posargs or ["--cov"]
-    session.install("uv")
-    session.run("uv", "pip", "install", ".[test]")
-    session.run("uv", "pip", "install", "torchvision")
-
+    session.install("-e .[test]", "torchvision")
     session.run("pytest", *args)
