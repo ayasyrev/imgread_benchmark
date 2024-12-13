c111dcee811efd403e6f95d4b432a232f7ae6cc2
ayasyrev <a.yasyrev@gmail.com>
2024-12-13 20:06:58 +0300

    add imread

--- src/imgread_benchmark/img_libs/img_libs_pkgs.py
+++ src/imgread_benchmark/img_libs/img_libs_pkgs.py
@@ -7,6 +7,7 @@ lib_to_package = {
     "cv2": "opencv-python-headless",  # conda - opencv
     "skimage": "scikit-image",  # conda
     "imageio": "imageio",  # conda
+    "imread": "imread",  # conda
     "kornia": "kornia",
     # "pyvips": "pyvips",  # conda
     "torchvision": "torchvision",
