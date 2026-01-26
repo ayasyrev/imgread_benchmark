df30b1a01a81a3d07d8c8e95083763acbf78b718
ayasyrev <a.yasyrev@gmail.com>
2026-01-26 18:25:40 +0300

    fix name

--- noxfile_img_libs.py
+++ noxfile_img_libs.py
@@ -2,7 +2,7 @@ import nox
 
 
 @nox.session(python=["3.10", "3.11", "3.12", "3.13", "3.14"], venv_backend="uv")
-def conda_tests(session):
+def run_tests(session):
     args = session.posargs or ["--cov"]
     session.install(
         "-e .",
